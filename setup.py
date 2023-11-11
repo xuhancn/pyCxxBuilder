e48d19c636a66cdc70f412beacb5d5c2c8a3641f
Xu Han <xu.han@outlook.com>
2023-11-11 13:46:25 +0800

    update code

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
+import setuptools.command.install
 from setuptools import setup
+import shutil
 import os
 
 PACKAGE_NAME = "CxxBuilder"
@@ -16,6 +18,52 @@ def _build_installation_dependency():
 
 ext_modules = []
 
+class install(setuptools.command.install.install):
+    def run(self):
+        super().run()
+
+
+class clean(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import glob
+        import re
+
+        with open(".gitignore") as f:
+            ignores = f.read()
+            pat = re.compile(r"^#( BEGIN NOT-CLEAN-FILES )?")
+            for wildcard in filter(None, ignores.split("\n")):
+                match = pat.match(wildcard)
+                if match:
+                    if match.group(1):
+                        # Marker is found and stop reading .gitignore.
+                        break
+                    # Ignore lines which begin with '#'.
+                else:
+                    # Don't remove absolute paths from the system
+                    wildcard = wildcard.lstrip("./")
+
+                    for filename in glob.glob(wildcard):
+                        try:
+                            os.remove(filename)
+                        except OSError:
+                            shutil.rmtree(filename, ignore_errors=True)
+
+cmdclass = {
+    # "bdist_wheel": wheel_concatenate,
+    # "build_ext": build_ext,
+    "clean": clean,
+    "install": install,
+    # "sdist": sdist,
+}
+
 setup(
     name=PACKAGE_NAME,
     version=cxx_builder_build_version,
@@ -29,7 +77,7 @@ setup(
     package_data={PACKAGE_NAME: ["*.so", "lib/*.so", "bin/*.dll", "lib/*.lib"]},
     zip_safe=False,
     ext_modules=ext_modules,
-    # cmdclass=cmdclass,
+    cmdclass=cmdclass,
     # entry_points=entry_points,
     license="https://www.apache.org/licenses/LICENSE-2.0",
     classifiers=[
