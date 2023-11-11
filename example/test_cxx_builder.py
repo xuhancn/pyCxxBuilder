d5b359a3c180afc851a1d561d779143c76eb1e33
Xu Han <xu.han@outlook.com>
2023-11-11 19:50:37 +0800

    update code.

--- example/test_cxx_builder.py
+++ example/test_cxx_builder.py
@@ -1,4 +1,27 @@
+import os
 import CxxBuilder as cb
 
-new_target = cb.CxxTarget()
-new_target.build()
+def get_files_in_dir(dir_path):
+    file_list = []
+    for file in os.listdir(dir_path):
+        file_path = (os.path.join(dir_path, file))
+        file_list.append(file_path)
+
+    return file_list    
+
+def build_libxsmm():
+    curr_dir = os.path.dirname(os.path.abspath(__file__))
+    libxsmm_root = os.path.join(curr_dir, "third_party", "libxsmm")
+
+    source_files = get_files_in_dir(os.path.join(libxsmm_root, "src"))
+    include_files = get_files_in_dir(os.path.join(libxsmm_root, "include"))
+
+    cxx_target = cb.CxxBuilder.BuildTarget()
+    cxx_target.target("libxsmm", sources = source_files, include_dirs= include_files)
+    cxx_target.build()
+
+    return
+
+
+if __name__=='__main__':
+    build_libxsmm()
